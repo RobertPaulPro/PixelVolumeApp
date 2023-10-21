ac3e5771e7e828f244c31bb44b5c1151472c0ada
RobertPaulPro <robertpaulprophoto@gmail.com>
2023-10-21 16:13:03 -0400

    created gunicorn file

---
+++ gunicorn_config.py
@@ -0,0 +1,4 @@
+bind = '0.0.0.0:8000'  # Listen on all available network interfaces, port 8000
+workers = 4  # Number of worker processes
+accesslog = '-'  # Log to stdout
+errorlog = '-'   # Log to stdout
